8b78599f39d43493845121add6f2d0dc6cc1d9d1
shoma <shomasdutt@gmail.com>
2022-01-30 20:34:29 +0000

    landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,40 +6,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../scss/app.css">
+    <link rel="stylesheet" href="{{ URL('scss/app.css')}}">
     <title>Tutor</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">MathsTutor</a>
+            <a class="navbar-brand" href="#">
+
+                <img src="{{ URL('images/logo.png')}}" id="logo" class="img-fluid" alt="logo">
+
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Testimonials</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Contact</a>
                     </li>
                 </ul>
 
             </div>
         </div>
     </nav>
-    <div class="container-fluid px-0" id="divided">
-        <div class="container px-0" id="tutorImage">
-            <img src="{{ URL('images/tutor.jpg')}}" class="img-fluid" alt="tutor">
-        </div>
-        <div class="container px-0" id="pattern">
-            <img src="{{ URL('images/tutor.jpg')}}" class="img-fluid" alt="tutor">
-        </div>
+
+    <div class="container-fluid" id="tutorImageDiv">
+
+        <img src="{{ URL('images/t1.jpg')}}" id="tutorImage" class="img-fluid" alt="tutor">
+
+    </div>
+
+    <div class="container-fluid" id="tutorIntro">
+        <p id="intro">
+            Hi, I'm Christopher Phan, and I teach Maths. I've been teaching for the last 25 years.
+            I'm based in North London, but I can easily travel to any part.
+        </p>
+
+        <p id="intro">
+            I have extensive experience teaching in some of the UK's top schools and offer unparalleled
+            insight into and each and every stage of a student's development.
+        </p>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
