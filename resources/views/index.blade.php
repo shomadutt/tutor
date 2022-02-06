d96bad919abcc694c1be41e0fc3fe45e2aa3ff23
shoma <shomasdutt@gmail.com>
2022-02-06 15:09:16 +0000

    sections added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,39 +39,45 @@
         </div>
     </nav>
 
+
     <div class="container-fluid px-0" id="landingDiv">
         <img src="{{ URL('images/maths.jpg')}}" alt="maths image" id="maths">
     </div>
 
     <div class="container-fluid px-0" id="introDiv">
-        <div class="col">
-            <p class="heading" id="about">
-                About Me
-            </p>
+        <section>
+            <div class="row">
+                <div class="col">
+                    <p class="heading" id="about">
+                        About Me
+                    </p>
 
-            <p id="tutorIntro">
-                Hi, I'm Christopher Phan, and I teach Maths. I've been teaching for the last 25 years.
-                I'm based in North London, but I can easily travel to any part. I have extensive experience
-                teaching in some of the UK's top schools and offer unparalleled
-                insight into and each and every stage of a student's development.
-            </p>
-        </div>
+                    <p id="tutorIntro">
+                        Hi, I'm Christopher Phan, and I teach Maths. I've been teaching for the last 25 years.
+                        I'm based in North London, but I can easily travel to any part. I have extensive experience
+                        teaching in some of the UK's top schools and offer unparalleled
+                        insight into and each and every stage of a student's development.
+                    </p>
+                    <p id="tutorIntro">
+                        After graduating with a Mathematics degree from Oxford University, I went on to complete my
+                        Teach First qualification at Berkshire Primary School, specialising in teaching Maths to Years 5 and 6.
+                        I was selected as part of Teach First's Associate Tutor team, delivering professional development sessions
+                        to Teach First participants. I am a highly experienced 7+, 11+, 13+, GCSE, A-Level and university entrance
+                        tutor.
+                    </p>
+                </div>
 
 
-        <div class="col tutorDiv">
-            <img src="{{ URL('images/tutor.jpg')}}" id="tutorImg" class="img-fluid" alt="tutor">
-        </div>
-
+                <div class="col tutorDiv">
+                    <img src="{{ URL('images/tutor.jpg')}}" id="tutorImg" class="img-fluid" alt="tutor">
+                </div>
+            </div>
+        </section>
     </div>
 
-    <!-- <div class="aboutDiv g-col-6 g-col-md-4">
-            <p id="tutorPara">
-                After graduating with a Mathematics degree from Oxford University, I went on to complete my
-                Teach First qualification at Berkshire Primary School, specialising in teaching Maths to Years 5 and 6.
-                I was selected as part of Teach First's Associate Tutor team, delivering professional development sessions
-                to Teach First participants. I am a highly experienced 7+, 11+, 13+, GCSE, A-Level and university entrance
-                tutor.
-            </p>
+    <div class="container-fluid px-0" id="aboutDiv">
+        <section id="aboutTutor">
+
             <p id="tutorIntro">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ipsum lorem, dignissim eget est vel, feugiat
                 iaculis magna. Nulla hendrerit congue turpis, sit amet tristique quam consectetur at. Pellentesque sagittis
@@ -85,9 +91,11 @@
                 massa arcu vehicula dui, efficitur egestas risus tortor id tortor. Integer est diam, tempor non fermentum vitae,
                 accumsan sed quam. Aliquam commodo congue urna, a porta risus porttitor sed.
             </p>
-        </div>
+        </section>
+    </div>
 
-        <div class="testimonialsDiv g-col-6 g-col-md-4">
+    <div class="container-fluid px-0" id="testimonialsDiv">
+        <section>
             <p class="heading" id="testimonials">
                 Testimonials
             </p>
@@ -117,38 +125,41 @@
                 Suspendisse gravida lobortis pulvinar. Quisque a dui suscipit, interdum orci sit amet, tincidunt tellus. Quisque euismod neque
                 ut scelerisque convallis. Quisque et viverra turpis.
             </p>
-        </div>
+        </section>
+    </div>
 
-        <div class="contactDiv g-col-6 g-col-md-4">
-            <div class="container-fluid py-0" id="contactDiv">
-                <p class="heading" id="contact">
-                    Contact Me
-                </p>
-                <form>
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
-                        <input type="name" class="form-control" id="name" />
+    <div class="container-fluid px-0" id="contactDiv">
+        <section>
+            <p class="heading" id="contact">
+                Contact Me
+            </p>
+            <form>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="name" class="form-control" id="name" />
 
-                    </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="email" />
-                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" />
+                </div>
 
-                    <div class="mb-3">
-                        <label for="message" class="form-label">Message</label>
-                        <textarea class="form-control" id="message" rows="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn" id="submitButton">Submit</button>
-                </form>
-            </div>
-        </div>
+                <div class="mb-3">
+                    <label for="message" class="form-label">Message</label>
+                    <textarea class="form-control" id="message" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn" id="submitButton">Submit</button>
+            </form>
+        </section>
+    </div>
 
-        <div class="footerDiv g-col-6 g-col-md-4">
+    <div class="container-fluid px-0" id="footerDiv">
+        <section>
             <footer>
                 <p id="copyright">mathstutorchris.com © 2022</p>
             </footer>
-        </div> -->
+        </section>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
